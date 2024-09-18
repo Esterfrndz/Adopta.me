df36cdcbbc4c107beffdfc57c5425a7b1c7b0896
Ester <esterffernandezz@gmail.com>
2024-09-18 19:03:35 +0200

    Navbar & First Section

--- src/components/PrimeraSection.js
+++ src/components/PrimeraSection.js
@@ -7,40 +7,22 @@ import Perro from '../assets/home/perro-def.png';
 
 
 
+
 function PrimeraSection(){
 
 return(
 
-<div className="d-flex justify-content-center align-items-center content">
-          <div className="row">
-            {/* Texto */}
-            <div className='col-md-4 contentText text-center text-md-start'>
-              <h2>
-                <strong>Encuentra tu mejor compañía</strong>
-              </h2>
-              <p>
-                Bienvenid@ a Adopta.me, la plataforma líder en adopción de perros y gatos. Descubra una amplia gama de adorables animales que esperan su hogar definitivo. Únase a nosotros para hacer una diferencia en sus vidas.
-              </p>
-              <div className='contentInput d-flex justify-content-center justify-content-md-start'>
-                <input type="text" className="form-control me-2" placeholder="Introduce tu correo"/>
-                <button className="btn btn-primary">Únete ahora</button>
-              </div>
-            </div>
+<div class="row-container">
+  <div class="left-section contentText">
+    <h2>Encuentra tu mejor compañía</h2>
+    <p>Bienvenid@ a Adopta.me, la plataforma líder en adopción de perros y gatos. Descubra una amplia gama de adorables animales que esperan su hogar definitivo. Únase a nosotros para hacer una diferencia en sus vidas.</p>
+    <button class="btn btn-primary">ADOPTA</button>
+  </div>
+  <div class="right-section">
+    <img src={Perro} alt="Descripción de la imagen"/>
+  </div>
+</div>
 
-            
-
-            {/* Imagen */}
-            <div className='col-12 col-md-8 d-flex justify-content-center align-items-center imagen '>
-
-              
-                 <img src={Perro} className='Perro img-fluid '/>
-             
-           
-            </div>
-          </div>
-        </div>
-
-    
 );
 
 
